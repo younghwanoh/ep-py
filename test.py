0574055b2ee921180b77ebfeb5f445e226805ca8
Young H. Oh <garion9013@gmail.com>
2014-08-29 18:52:04 +0900

    minor add test module's option

--- test.py
+++ test.py
@@ -2,10 +2,15 @@
 
 import os
 from time import sleep
+import sys
 
 tests = ["line-key","line-raw","line-norm","bar-clustered",
          "bar-norm-clustered","bar-key-clustered","bar-key-cc","bar-single",
          "box-key","box-time","getter-test"]
+
+if sys.argv[1] == "-r":
+    os.system('rm *.pdf')
+    sys.exit();
 
 for i in tests:
     print("\033[1;31m%s\033[m test !! ============================================" % i)
