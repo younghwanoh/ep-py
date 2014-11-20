158c43fb334a8497bc96e6a0eb403901cf6ced35
yongjunlee <yongjunlee@YJ-iMac.local>
2014-11-20 15:58:19 +0900

    wgew

--- examples/multi-program-line.py
+++ examples/multi-program-line.py
@@ -33,7 +33,7 @@ if bool(args.style) == True:
 # Parse ======================================================================
 
 # Polybench
-PP = ep.PatternParser(ep.tRead("../dat/cbar-line/poly.dat"))
+PP = ep.PatternParser(ep.tRead("../dat/cbar-line/multi-program.dat"))
 PP.PickKeyWith("row")
 PP.ParseWith("\t")
 PP.datNormTo("col1", "col2", select="min")
@@ -41,37 +41,41 @@ PP.datNormTo("col1", "col2", select="min")
 PD = []
 PD.append(ep.Group(PP, "col1", color=mc["white"], hatch=""))
 PD.append(ep.Group(PP, "col2", color=mc["ddwhite"], hatch=""))
-PD.append(ep.Group(PP, "col3", color=mc["black"], hatch=""))
+PD.append(ep.Group(PP, "col3", color=mc["red"], hatch=""))
+PD.append(ep.Group(PP, "col4", color=mc["green"], hatch=""))
+PD.append(ep.Group(PP, "col5", color=mc["black"], hatch=""))
 
-PD[0].setLegend("FluidiCL")
-PD[1].setLegend("Boyer et al.")
-PD[2].setLegend("jAWS")
+PD[0].setLegend("off")
+PD[1].setLegend("bank")
+PD[2].setLegend("sram")
+PD[3].setLegend("tlb")
+PD[4].setLegend("\"ideal\"")
 
-# WebCL
-PP = ep.PatternParser(ep.tRead("../dat/cbar-line/webcl.dat"))
-PP.PickKeyWith("row")
-PP.ParseWith("\t")
-PP.datNormTo("col1", "col2", select="min")
-
-WD = []
-WD.append(ep.Group(PP, "col1", color=mc["white"], hatch=""))
-WD.append(ep.Group(PP, "col2", color=mc["ddwhite"], hatch=""))
-WD.append(ep.Group(PP, "col3", color=mc["black"], hatch=""))
-
-# Geomean
-PP = ep.PatternParser(ep.tRead("../dat/cbar-line/geomean-best.dat"))
-PP.PickKeyWith("row")
-PP.ParseWith("\t")
-
-GD = []
-GD.append(ep.Group(PP, "col1", color=mc["white"], hatch=""))
-GD.append(ep.Group(PP, "col2", color=mc["ddwhite"], hatch=""))
-GD.append(ep.Group(PP, "col3", color=mc["black"], hatch=""))
+#  # WebCL
+#  PP = ep.PatternParser(ep.tRead("../dat/cbar-line/webcl.dat"))
+#  PP.PickKeyWith("row")
+#  PP.ParseWith("\t")
+#  PP.datNormTo("col1", "col2", select="min")
+#  
+#  WD = []
+#  WD.append(ep.Group(PP, "col1", color=mc["white"], hatch=""))
+#  WD.append(ep.Group(PP, "col2", color=mc["ddwhite"], hatch=""))
+#  WD.append(ep.Group(PP, "col3", color=mc["black"], hatch=""))
+#  
+#  # Geomean
+#  PP = ep.PatternParser(ep.tRead("../dat/cbar-line/geomean-best.dat"))
+#  PP.PickKeyWith("row")
+#  PP.ParseWith("\t")
+#  
+#  GD = []
+#  GD.append(ep.Group(PP, "col1", color=mc["white"], hatch=""))
+#  GD.append(ep.Group(PP, "col2", color=mc["ddwhite"], hatch=""))
+#  GD.append(ep.Group(PP, "col3", color=mc["black"], hatch=""))
 
 # label lists
-poly_list = ["ATAX", "BICG", "SYRK", "SYR2K", "GEMM", "2MM", "CORR"]
-webcl_list = ["Mandelbrot", "Nbody", "Sobel-CorG", "Random"]
-geo_list = ["geomean"]
+poly_list = ["HM1", "HM2", "HM3", "HM4", "HM4", "HM5", "HM6", "HM7", "HM8"]
+#webcl_list = ["Mandelbrot", "Nbody", "Sobel-CorG", "Random"]
+#geo_list = ["geomean"]
 
 
 # Draw ======================================================================
@@ -80,7 +84,8 @@ CB = ep.CBarPlotter(ylabel="Speedup over Best Device", ylpos=[-.035, 0.5],
                     width=30, height=6.8)
 
 # Set Ticks
-L1 = ep.TickLabel(None, poly_list + webcl_list + geo_list)
+#L1 = ep.TickLabel(None, poly_list + webcl_list + geo_list)
+L1 = ep.TickLabel(None, poly_list)
 CB.setTicks(yspace=[0, 0.5, 1, 1.5], label=L1)
 CB.annotate(["Polybench", "WebKit-WebCL"], [[27.5, -.30], [85, -.30]], fontsize=30)
 
@@ -91,9 +96,9 @@ CB.setFigureStyle(ylim=[0, 1.5], bottomMargin=0.18, fontsize=25,
 
 CB.draw(*PD, barwidth=2)
 CB.setBaseOffset(14)
-CB.draw(*WD, barwidth=2)
-CB.setBaseOffset(14)
-CB.draw(*GD, barwidth=2)
+#CB.draw(*WD, barwidth=2)
+#CB.setBaseOffset(14)
+#CB.draw(*GD, barwidth=2)
 
 g_base = CB.getGlobalBase()
 
@@ -105,36 +110,36 @@ marker = "o"
 
 # Parse ======================================================================
 # Polybench
-PP = ep.PatternParser(ep.tRead("../dat/cbar-line/poly-line.dat"))
+PP = ep.PatternParser(ep.tRead("../dat/cbar-line/multi-program-line.dat"))
 PP.PickKeyWith("col")
 PP.ParseWith("\t")
 
 PLB = []
 for i, val in enumerate(poly_list):
-    PLB.append(ep.Group(PP, [g_base[i], g_base[i]+2, g_base[i]+4, g_base[i]+6, g_base[i]+8 ], val,
+    PLB.append(ep.Group(PP, [g_base[i], g_base[i]+2, g_base[i]+4], val,
                             color=color, face=face, marker=marker))
 PLB[0].setLegend("Load Balance Factor")
 
-# WebCL
-PP = ep.PatternParser(ep.tRead("../dat/cbar-line/webcl-line.dat"))
-PP.PickKeyWith("col")
-PP.ParseWith("\t")
-
-WLB = []
-for i, val in enumerate(webcl_list):
-    WLB.append(ep.Group(PP, [g_base[i], g_base[i]+2, g_base[i]+4], val,
-                            color=color, face=face, marker=marker))
-
-# Geomean
-PP = ep.PatternParser(ep.tRead("../dat/cbar-line/geomean-line.dat"))
-PP.PickKeyWith("col")
-PP.ParseWith("\t")
-
-GLB = []
-for i, val in enumerate(geo_list):
-    GLB.append(ep.Group(PP, [g_base[i], g_base[i]+2, g_base[i]+4], val,
-                            color=color, face=face, marker=marker))
-
+# # WebCL
+# PP = ep.PatternParser(ep.tRead("../dat/cbar-line/webcl-line.dat"))
+# PP.PickKeyWith("col")
+# PP.ParseWith("\t")
+# 
+# WLB = []
+# for i, val in enumerate(webcl_list):
+#     WLB.append(ep.Group(PP, [g_base[i], g_base[i]+2, g_base[i]+4], val,
+#                             color=color, face=face, marker=marker))
+# 
+# # Geomean
+# PP = ep.PatternParser(ep.tRead("../dat/cbar-line/geomean-line.dat"))
+# PP.PickKeyWith("col")
+# PP.ParseWith("\t")
+# 
+# GLB = []
+# for i, val in enumerate(geo_list):
+#     GLB.append(ep.Group(PP, [g_base[i], g_base[i]+2, g_base[i]+4], val,
+#                             color=color, face=face, marker=marker))
+# 
 # Draw ======================================================================
 
 # get duplicated axis from previous plotter
