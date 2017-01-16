8bcc8210cb8d1ae8f1644b555d38e5f8865d2b67
Young H. Oh <garion9013@gmail.com>
2017-01-16 16:48:39 +0900

    force save to pdf for headless environments

--- epic/plotter.py
+++ epic/plotter.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib
+matplotlib.use('pdf')
+import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from tools import tTranspose, tMergeCrossSpace, tCheckArgsExists
 
